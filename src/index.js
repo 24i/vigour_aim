f2af7e205d7f18a4843a3483a089752976a8af99
Youri Daamen <me@youz.io>
2017-05-14 13:42:51 +0200

    setup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-const fm = {
+const focusManager = {
+  currentFocus: false,
+  map: {},
   /*
     register element, this can happen on eg. render
+    params:
+    - coordinates (obj) eg [0,0,0]
+    - set (obj) eg { state, x, y, focusIn, focusUpdate, focusOut }
   */
   register (coordinates, set) {
 
   },
   /*
     unregister element, this can happen on eg. remove
+    params:
+    - coordinates (obj) eg [0,0,0]
   */
   unregister (coordinates) {
 
   },
   /*
     unregister element, this can happen on eg. remove
+    params:
+    - coordinates (obj) eg [0,0,0]
+    - set (obj) eg { x }
   */
   update (coordinates, set) {
 
   }
 }
 
-export default fm
+export default focusManager
