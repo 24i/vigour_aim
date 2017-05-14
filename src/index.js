a121d1dabd23f9569d8a01c45df326f577c41586
Youri Daamen <me@youz.io>
2017-05-14 13:04:32 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const fm = {
+  /*
+    register element, this can happen on eg. render
+  */
+  register (coordinates, set) {
+
+  },
+  /*
+    unregister element, this can happen on eg. remove
+  */
+  unregister (coordinates) {
+
+  },
+  /*
+    unregister element, this can happen on eg. remove
+  */
+  update (coordinates, set) {
+
+  }
+}
+
+export default fm
